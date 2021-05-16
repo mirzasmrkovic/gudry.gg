4d00e8a6983dfa2fd97b795eac45b163165ebd2c
Mirza Smrkovic <mirza.smrkovic@gmail.com>
2021-05-16 12:49:20 +0200

    minor 'connect' function-refactoring and rename it to 'connection'

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,14 +1,19 @@
 import mongoose from 'mongoose'
-const options = {
-  dbUrl: 'mongodb://localhost:27017/restaurant-api'
-}
+// const options = {
 
-export const connect = (url = options.dbUrl, opts = {}) => {
-  return mongoose.connect(url, {
-    ...opts,
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-  })
+// }
+
+export const connection = async (url, opts = {}) => {
+  try {
+    return await mongoose.connect(url, {
+      ...opts,
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+    })
+  } catch (e) {
+    console.error(e)
+    throw e
+  }
 }
