43f74fa35ec35a85349590a2bdd455d479815a7e
Mirza Smrkovic <mirza.smrkovic@gmail.com>
2021-06-02 14:22:42 +0200

    add user schema

---
+++ server/resources/user/user.model.js
@@ -0,0 +1,14 @@
+import { Schema, model } from 'mongoose'
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  steamid: {
+    type: String,
+    required: true
+  }
+})
+
+export const User = model('User', userSchema)
